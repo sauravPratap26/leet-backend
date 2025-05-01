a0ba74294cd76cf68bc3fec48fae11f5b2124a26
Saurav Singh <sauravcloud26@gmail.com>
2025-05-01 12:46:07 +0530

    sample env provided and sample server tested

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hell0")
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config({path:"./.env"})
+const app = express();
+
+const PORT = process.env.PORT||5000
+app.listen(PORT,()=>{
+    console.log(`server is up and running on port ${PORT}`)
+})
