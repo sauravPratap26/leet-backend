8fa88f96de4495d1850470b78f6d19a7f0dc5143
Saurav Singh <sauravcloud26@gmail.com>
2025-05-04 16:26:41 +0530

    problem router and controller boilerplate code written

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,8 @@
+import asyncHandler from "../utils/async-handler.js";
+
+export const createProblem = asyncHandler(async (req, res) => {});
+export const getAllProblem = asyncHandler(async (req, res) => {});
+export const getProblemById = asyncHandler(async (req, res) => {});
+export const updateProblemById = asyncHandler(async (req, res) => {});
+export const deleteProblem = asyncHandler(async (req, res) => {});
+export const getAllProblemsSolvedByUser = asyncHandler(async (req, res) => {});
--- src/routes/problem.routes.js
+++ src/routes/problem.routes.js
@@ -1,7 +1,25 @@
 import { Router } from "express";
 import { authMiddleware, checkAdmin } from "../middlewares/auth.middleware.js";
-const router =Router()
+import {
+    createProblem,
+    deleteProblem,
+    getAllProblem,
+    getAllProblemsSolvedByUser,
+    getProblemById,
+    updateProblemById,
+} from "../controllers/problem.controller.js";
+const router = Router();
 
-router.post("/create-problem",authMiddleware,checkAdmin)
+router.post("/create-problem", authMiddleware, checkAdmin, createProblem);
+router.post("/get-all-problem", authMiddleware, getAllProblem);
+router.post("/get-problem/:id", authMiddleware, getProblemById);
+router.post(
+    "/update-problem/:id",
+    authMiddleware,
+    checkAdmin,
+    updateProblemById,
+);
+router.post("/delete-problem/:id", authMiddleware, checkAdmin, deleteProblem);
+router.post("/get-solved-problems", authMiddleware, getAllProblemsSolvedByUser);
 
-export default router 
+export default router;
